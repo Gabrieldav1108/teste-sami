f1ec065c3e28acbb5ed1182d8521bd2fac3e2693
Gabriel Davila <gabrielsdavila2@gmail.com>
2025-11-14 02:29:45 -0300

    delete people

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        app()->setLocale('pt_BR');
+        FacadesValidator::excludeUnvalidatedArrayKeys();
+
     }
 }
